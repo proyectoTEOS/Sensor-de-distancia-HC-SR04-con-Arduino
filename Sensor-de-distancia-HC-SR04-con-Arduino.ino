5c23b6531f8f28c3b9ca5913ae2147206dce0e4c
TEOS <proyectoteos@gmail.com>
2018-01-15 17:32:12 -0600

    project code
    First version of the project

---
+++ Sensor-de-distancia-HC-SR04-con-Arduino.ino
@@ -0,0 +1,32 @@
+/*
+  Created by TEOS
+  Domotic with Arduino https://goo.gl/btZpjs
+  YouTube https://goo.gl/k7TUSZ
+  Instagram https://goo.gl/zEIjia
+  Facebook https://goo.gl/eivraR
+  Twitter https://goo.gl/du5Wgn
+  Github https://goo.gl/Xl5IiS
+  Google Plus https://goo.gl/gBnUdh
+  WEB https://goo.gl/jtiYJy
+*/
+
+#include <Ultrasonic.h>
+int distanceT, finalDistanceT, sumT = 0;
+
+Ultrasonic ultrasonicT(9,8); 
+
+void setup() {
+  Serial.begin(9600); 
+}
+
+void loop(){
+  for(int iT = 0; iT<=10; iT+=1){
+    sumT = sumT + (distanceT = ultrasonicT.Ranging(CM));
+    delay(70);
+  }
+  finalDistanceT = sumT/10;
+  sumT = 0;
+  Serial.print("La distancia actual es de ");
+  Serial.print(finalDistanceT);
+  Serial.println(" CM");
+}
